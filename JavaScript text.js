31e29756463687b518e7fbcf59c5e15ad0892163
clombard99 <57107257+clombard99@users.noreply.github.com>
2019-11-08 15:20:32 -0500

    Add files via upload

---
+++ JavaScript text.js
@@ -0,0 +1,29 @@
+
+function changeBackground(){
+    
+    document.getElementById("para").style.backgroundColor ="red";
+
+}
+
+function Normal(){
+    
+    document.getElementById("para").style.backgroundColor ="transparent";
+    
+}
+
+
+
+
+
+function changeStyle() {
+    
+     document.getElementById ("para1").style.color ="blue";
+}
+
+function backToNormal(){
+    document.getElemntById ("para1").style.color ="transparent";
+}
+function changeStyle() {
+var paragraph = document.getElementByTagName("p");
+var changeParaText =paragraph(1).style.fontStyle ="italic";
+}
